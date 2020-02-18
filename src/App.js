ff6788ec3259b2b797e8801f786becb9f67ecefa
Traci Crawford <tracicrawford@Tracis-MacBook-Air.local>
2020-02-17 18:20:44 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './reset.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      menuIsOpen: false
+    }
+  }
+  handleClick = () => {
+    this.setState ({menuIsOpen: !this.state.menuIsOpen})
+  }
+  render () {
+    return (
+      <div>
+        <nav>
+          <h2 className='start'>Start Bootstrap</h2>
+          <ul className="nav-links">
+            <li>SERVICES</li>
+            <li>PORTFOLIO</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+          </ul>
+
+          <section className='menbtn' onClick={this.handleClick}>
+            <h3>MENU</h3>
+            <p className='bar1'></p>
+            <p className='bar2'></p>
+            <p className='bar3'></p>
+            {/*<img
+            onClick={this.handleClick}
+            width="50px"
+            src="https://i.ya-webdesign.com/images/hamburger-menu-icon-png-white-6.png" />*/}
+          </section>
+        </nav>
+        <menu className={this.state.menuIsOpen === true ? "menu-open" : ""}>
+          <ul>
+            <li>SERVICES</li>
+            <li>PORTFOLIO</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+          </ul>
+        </menu>
+      </div>
+    )
+  }
 }
 
 export default App;
