37b950b96fd8302c589280f6ab6fced26f69c1bb
Traci Crawford <tracicrawford@Tracis-MacBook-Air.local>
2020-02-17 19:22:50 -0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -27,13 +27,11 @@ class App extends React.Component {
 
           <section className='menbtn' onClick={this.handleClick}>
             <h3>MENU</h3>
-            <p className='bar1'></p>
-            <p className='bar2'></p>
-            <p className='bar3'></p>
-            {/*<img
-            onClick={this.handleClick}
-            width="50px"
-            src="https://i.ya-webdesign.com/images/hamburger-menu-icon-png-white-6.png" />*/}
+            <div className='bars'>
+              <p></p>
+              <p></p>
+              <p></p>
+            </div>
           </section>
         </nav>
         <menu className={this.state.menuIsOpen === true ? "menu-open" : ""}>
